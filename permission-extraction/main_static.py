4065270f04c78f1bb47b4f8c78025df47eb7cff2
vz000 <vrosasz1700@alumno.ipn.mx>
2022-11-12 10:58:51 -0600

    Add further description and self notes.

--- permission-extraction/main_static.py
+++ permission-extraction/main_static.py
@@ -8,6 +8,10 @@ pprint(apkList)
 
 # In this part of the solution, we need to read the permissions, which are located inside
 # of AndroidManifest.xml. Sources are not needed.
+
+# In normal execution it is not expected to extract all the permissions from the complete apk
+# list. So the following loop may be removed when integrated with the next step/process, and 
+# after the training phase.
 for apk in apkList:
     apkLocation = folderName + apk
     os.system('apktool d -s ' + apkLocation + ' -o permissionsExtraction -f')
