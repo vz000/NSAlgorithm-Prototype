1e40c74f547a39827ce14ab78a092d9024ea47b8
vz000 <vrosasz1700@alumno.ipn.mx>
2023-02-15 17:29:10 -0600

    Script fix to get permissions

--- permission-extraction/main_static.py
+++ permission-extraction/main_static.py
@@ -5,22 +5,27 @@ import xml.etree.ElementTree as ET
 # To simulate the real case. Normal apps are stored inside of /data/app/
 folderName = "./data/app/"
 apkList = os.listdir(folderName)
+print(len(apkList))
 
 # In this part of the solution, we need to read the permissions, which are located inside
 # of AndroidManifest.xml. Sources are not needed.
-# ---- Evaluate decompiling .dex files
-apk = apkList[0] # one .apk 
-apkLocation = folderName + apk
-os.system('apktool d -s ' + apkLocation + ' -o ./data/unpackaged -f')
-manifestTree = ET.parse('./data/unpackaged/AndroidManifest.xml')
-manifestRoot = manifestTree.getroot()
-appPermissions = []
-for manifestChild in manifestRoot:
-    if manifestChild.tag == 'uses-permission' or manifestChild.tag == 'permission':
-        manifestChildAttr = list(manifestChild.attrib.keys())
-        permission = manifestChild.attrib[manifestChildAttr[0]].split(".")
-        appPermissions.append(permission[-1])
 
-with open('./data/unpackaged/permissions.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(appPermissions)
+for apk_number in range(0,len(apkList)):
+    apkLocation = folderName + apkList[apk_number]
+    try:
+        os.system('apktool d -s ' + apkLocation + ' -o ./data/unpackaged -f')
+        manifestTree = ET.parse('./data/unpackaged/AndroidManifest.xml')
+        manifestRoot = manifestTree.getroot()
+        appPermissions = []
+        for manifestChild in manifestRoot:
+            if manifestChild.tag == 'uses-permission' or manifestChild.tag == 'permission':
+                manifestChildAttr = list(manifestChild.attrib.keys())
+                permission = manifestChild.attrib[manifestChildAttr[0]].split(".")
+                appPermissions.append(permission[-1])
+
+        with open('./data/permissions.csv', 'a+', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(appPermissions)
+    except:
+        print("Deleting APK...")
+        os.remove(apkLocation)
